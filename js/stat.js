31d22a8e7c8d40ecf20f3eddbc50ab134e2b71a5
GreeneTM <d.greenetm@gmail.com>
2018-11-17 18:20:15 +0300

    Добавляет тень под облако

--- js/stat.js
+++ js/stat.js
@@ -1,5 +1,6 @@
 window.renderStatistics = function (ctx, names, times) {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(110, 20, 420, 270);
   ctx.fillStyle = '#fff';
   ctx.fillRect(100, 10, 420, 270);
-
 };
