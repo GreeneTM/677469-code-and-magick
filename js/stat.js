128ce2588361198ebca535157220a6b14a95c0bd
GreeneTM <d.greenetm@gmail.com>
2018-11-18 18:55:55 +0300

    Учебный проект «Холст»

--- js/stat.js
+++ js/stat.js
@@ -7,33 +7,57 @@ var COLUMN_SPACING = 50; //расстояние между колонками
 var TEXT_HEIGHT = 20; //высота строки
 var FOND_GAP = 20; // отступ текста
 var BAR_WIDTH = 40; // ширина столбца
-
+var FIRST_INDENT = 40; // Первый отступ
+var MAX_HEIGHT = 150; // Максималная высота столбца
+// функция отрисовки облочка
 var renderCloud = function (ctx, x, y, color,) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+// функция нахождения максимального времени (getMaxElement)
+var getMaxElement = function (arr) {
+  var maxElement = arr[0];
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i]
+    }
+  }
+  return Math.round(maxElement);
+};
+// функция случайного оттенка синего цвета
+var randomColor = function(){
+  var allowed = "ABCDEF0123456789", S = "#0000";
+
+  while(S.length < 7){
+    S += allowed.charAt(Math.floor((Math.random()*16)+1));
+  }
+  return S;
 };
 
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
+  ctx.fillStyle = '#000';
+
+  var maxTime = getMaxElement(times);
+
   ctx.font = '16px PT Mono';
   ctx.fillStyle = 'black';
   ctx.fillText('Ура вы победили!', CLOUD_X + FOND_GAP, CLOUD_Y + GAP + TEXT_HEIGHT);
   ctx.fillText('Список результатов:', CLOUD_X + FOND_GAP, CLOUD_Y + GAP + TEXT_HEIGHT * 2);
 
-  ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  ctx.fillText('Вы', 140, 260);
-  ctx.fillRect(140, 240, 40, -150);
+  for (var i = 0; i < names.length; i++) {
 
-  ctx.fillStyle = 'blue';
-  ctx.fillText('Кира', 230, 260);
-  ctx.fillRect(230, 240, 40, -20);
-
-  ctx.fillText('Вика', 320, 260);
-  ctx.fillRect(320, 240, 40, -70);
-
-  ctx.fillText('Давид', 410, 260);
-  ctx.fillRect(410, 240, 40, -110)
-
+    ctx.fillStyle = '#000';
+    ctx.fillText(names[i], CLOUD_X + FIRST_INDENT + (BAR_WIDTH + COLUMN_SPACING) * i, CLOUD_HEIGHT - GAP);
+    ctx.fillText(Math.round(times[i]), CLOUD_X + FIRST_INDENT + (BAR_WIDTH + COLUMN_SPACING) * i, CLOUD_Y + CLOUD_HEIGHT - GAP * 3 - TEXT_HEIGHT - (MAX_HEIGHT * times[i]) / maxTime)
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = randomColor ();
+      }
+    ctx.fillRect(CLOUD_X + FIRST_INDENT + (BAR_WIDTH + COLUMN_SPACING) * i, CLOUD_Y + CLOUD_HEIGHT - GAP * 2 - TEXT_HEIGHT - (MAX_HEIGHT * times[i]) / maxTime, BAR_WIDTH, (MAX_HEIGHT * times[i]) / maxTime);
+  }
 };
