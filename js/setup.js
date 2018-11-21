a5fef97a39c802d460315b932a55c017b05fc19f
GreeneTM <d.greenetm@gmail.com>
2018-11-21 21:06:39 +0300

    Создает массив из 4 объектов

--- js/setup.js
+++ js/setup.js
@@ -1,2 +1,29 @@
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
+
+var wizards = [
+  {
+    name: ,
+    surname: ,
+    coatColor: ,
+    eyesColor:
+  },
+  {
+    name: ,
+    surname: ,
+    coatColor: ,
+    eyesColor:
+  },
+  {
+    name: ,
+    surname: ,
+    coatColor: ,
+    eyesColor:
+  },
+  {
+    name: ,
+    surname: ,
+    coatColor: ,
+    eyesColor:
+  }
+];
