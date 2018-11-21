0b000842519aa7f4b822d6d976b75bdc21513d74
GreeneTM <d.greenetm@gmail.com>
2018-11-21 21:58:07 +0300

    покажит блок похожих магов

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,8 @@
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
+document.querySelector('.setup-similar').classList.remove('hidden');
+
 var WIZARD_NAME = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
