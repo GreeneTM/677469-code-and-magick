bf0e70ef524caa7e8c665bce4dc81d67982e44ab
GreeneTM <d.greenetm@gmail.com>
2018-11-21 21:08:20 +0300

    Создаем массивы с данными

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,12 @@
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
+var WIZARD_NAME = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SURNAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARD_COAT_COLOR = [rgb(101, 137, 164), rgb(241, 43, 107), rgb(146, 100, 161), rgb(56, 159, 117), rgb(215, 210, 55), rgb(0, 0, 0)];
+var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+
+
 var wizards = [
   {
     name: ,
