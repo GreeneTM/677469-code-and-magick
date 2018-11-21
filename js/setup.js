71e105428e018994695913528c63320874d9ef2e
GreeneTM <d.greenetm@gmail.com>
2018-11-21 21:25:05 +0300

    Создает функцию случайного выбора из массива

--- js/setup.js
+++ js/setup.js
@@ -6,6 +6,9 @@ var WIZARD_SURNAME = ['да Марья', 'Верон', 'Мирабелла', 'В
 var WIZARD_COAT_COLOR = [rgb(101, 137, 164), rgb(241, 43, 107), rgb(146, 100, 161), rgb(56, 159, 117), rgb(215, 210, 55), rgb(0, 0, 0)];
 var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 
+var randomSelections = function (items) {
+  return items[Math.floor(Math.random()*items.length)];
+};
 
 var wizards = [
   {
